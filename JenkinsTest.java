1ba250d73bc222b60cdcbc17b2dadd7bda524fb1
Abhinav Umat <46773288+Abhinavumat95@users.noreply.github.com>
2023-08-01 12:05:59 -0400

    Update JenkinsTest.java

--- JenkinsTest.java
+++ JenkinsTest.java
@@ -3,7 +3,7 @@ public class JenkinsTest {
 
 	public static void main(String[] args) {
 		
-		System.out.println("This is me testing Jenkins. Test 1!");
+		System.out.println("This is me testing Jenkins. Test 1!")
 
 	}
 
